cde39d1dbe4a4ae182d3f25ddb8ff4151dacf96b
sunny <ricklee1998@naver.com>
2022-10-31 17:27:55 +0900

    로딩 기능 추가

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [list, setList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
   const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
   
   function mountData() {
@@ -15,6 +17,7 @@ export default function Home() {
     .then(res => {
       console.log(res.data)
       setList(res.data)
+      setIsLoading(false)
     })
   }
 
@@ -26,16 +29,28 @@ export default function Home() {
   return (
     <div>
       <Head><title>HOME | Ricky</title></Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-          베스트 상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(9)} />
+      
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   );
 }
--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,7 +1,35 @@
 import { useRouter } from 'next/router'
+import Axios from "axios";
+import Item from "../../src/component/Item";
+import Head from "next/head";
 
-export default function Id() {
+export default function Id( { item }) {
   const router = useRouter()
   const { id } = router.query
-  return <div>Post: {id}</div>
+  return (
+    <div>
+      {item && (
+        <div>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          <Item item={item} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      item: data,
+    },
+  };
 }
