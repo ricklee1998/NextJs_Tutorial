48260e77bf7cf55e81e96dc9c2833d1d26d8078d
sunny <ricklee1998@naver.com>
2022-10-31 15:52:22 +0900

    상품 리스트 추가

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
+import Axios from 'axios'
 import Head from 'next/head'
 import Image from 'next/image'
+import ItemList from '../src/component/ItemList'
+import { Divider, Header, Loader } from "semantic-ui-react";
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [list, setList] = useState([])
+  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+  
+  function mountData() {
+    Axios.get(API_URL)
+    .then(res => {
+      console.log(res.data)
+      setList(res.data)
+    })
+  }
+
+  useEffect(() => {
+    mountData()
+
+  }, [])
+
   return (
-    <div></div>>
-  )
+    <div>
+      <Head><title>HOME | Ricky</title></Head>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(0, 9)} />
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(9)} />
+    </div>
+  );
 }
